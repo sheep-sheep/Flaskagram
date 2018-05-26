54ce28029cfaeceb5060ed158e6d4cd152c7e57c
Yang <zhang.yang.frank@gmail.com>
2018-05-26 12:59:12 -0400

    bug for gunicorn "error: [Errno 98] Address already in use"

--- runserver.py
+++ runserver.py
@@ -8,7 +8,6 @@ if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    app.run(debug=debug)
 
 if __name__ == '__main__':
     app.run(debug=debug)
