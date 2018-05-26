8cd8f0d14cee95c8977f8e4a56c0c3e6834a2c5b
Yang <zhang.yang.frank@gmail.com>
2018-05-26 10:45:09 -0400

    logger for gunicorn

--- runserver.py
+++ runserver.py
@@ -1,6 +1,14 @@
 # -*- encoding: UTF-8 -*-
-
+import logging
 from flaskagram import app
 
+debug = True
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    app.run(debug=debug)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=debug)
